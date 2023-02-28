2a42da53951503aa909f612058e6f513502c9e93
WinsomeQuill <artemqa.belyakov07@gmail.com>
2023-02-28 10:58:10 +0700

    ListViewCellArticle is put out as separate object

---
+++ XamarinNews/XamarinNews/Elements/MainPage/ListViewCellPreviewArticle.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinNews.Elements.MainPage {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewCellPreviewArticle : ContentView {
+        public ListViewCellPreviewArticle() {
+            InitializeComponent();
+        }
+    }
+}
